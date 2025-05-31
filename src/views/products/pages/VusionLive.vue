794f21c222992fceeb032f8feb33c10eb3b4ba4f
Avinash A <avinash.a@labsquire.com>
2025-06-01 00:15:55 +0530

    Updated(ProductsOverview Page UI)

--- src/views/products/pages/VusionLive.vue
+++ src/views/products/pages/VusionLive.vue
@@ -1,245 +1,47 @@
 <template>
-    <div class="home">
-        <div class="product-hero-section">
-            <div class="mds-container mds-grid mds-grid-size-2">
-                <div class="product-hero-section-content mds-grid">
-                    <h1>Connect In-Store Operations with Data-Augmented Intelligence</h1>
-                    <p class="mds-font-size-lg mds-line-height-xl">
-                        VusionLive transforms in-store operations by connecting advanced IoT and data analytics with
-                        real-time activities in the aisles, delivering actionable insights that inform everyday
-                        processes, drive better operational efficiency, enhance store performance, and accelerate sales
-                        growth.
-                    </p>
-                </div>
-                <div class="product-hero-section-image">
-                    <img src="../../../assets/media/products/vusion-live-banner.png" alt="" />
-                </div>
+  <div class="home">
+    <div class="mds-section">
+      <div class="mds-container">
+        <div class="mds-flex mds-flex-column">
+          <div
+            class="mds-grid mds-grid-size-2 mds-gap-around-7xl mds-m-vertical-7xl"
+          >
+            <div class="mds-section-image mds-self-center">
+              <img
+                class="mds-border-radius-base"
+                src="https://www.vusion.com/wp-content/uploads/2024/12/031A9649-scaled-1-1540x1020-c-default.jpg"
+                alt=""
+              />
             </div>
-        </div>
-        <div class="product-retail">
-            <div class="mds-container">
-                <div class="mds-card">
-                    <div class="mds-grid mds-grid-size-2">
-                        <div class="product-retail-content">
-                            <div
-                                class="mds-m-bottom-lg mds-font-size-md-2 mds-text-transform-uppercase mds-letter-spacing-1 mds-font-weight-500">
-                                High-performance retail analytics
-                            </div>
-                            <h2 class="mds-m-bottom-lg mds-font-size-4xl">
-                                Transforming Retail Operations with Data and AI
-                            </h2>
-                            <p class="mds-font-size-base mds-font-weight-400 mds-line-height-xl">
-                                VusionLive is a comprehensive retail intelligence platform that uniquely integrates
-                                real-time IoT data — capturing precise in-store conditions — with sales figures, loyalty
-                                programs, and third-party intelligence. It provides actionable recommendations for
-                                optimizing pricing, assortment, and merchandising, while supporting their execution
-                                through integrated campaign management and operational tools.
-                            </p>
-                        </div>
-                        <div class="product-retail-image">
-                            <img src="../../../assets/media/products/vusion-live-transforming.jpg" alt="" />
-                        </div>
-                    </div>
+            <div class="mds-section-content mds-p-around-none">
+              <h2 class="mds-m-bottom-lg mds-font-size-5xl">Results</h2>
+              <div
+                class="mds-flex mds-flex-column mds-m-top-5xl mds-gap-around-xl mds-p-bottom-2xl mds-height-px-200"
+              >
+                <div class="mds-flex mds-items-center mds-gap-around-xl">
+                  <i class="pi pi-check mds-font-size-xl"></i>
+                  <p class="mds-font-weight-600 mds-font-size-lg">
+                    +12% Volume Growth compared to the same period last year.
+                  </p>
                 </div>
+                <div class="mds-flex mds-items-center mds-gap-around-xl">
+                  <i class="pi pi-check mds-font-size-xl"></i>
+                  <p class="mds-font-weight-600 mds-font-size-lg">
+                    Sales outperformed similar benchmarks by a factor of x7.
+                  </p>
+                </div>
+                <div class="mds-flex mds-items-center mds-gap-around-xl">
+                  <i class="pi pi-check mds-font-size-xl"></i>
+                  <p class="mds-font-weight-600 mds-font-size-lg">
+                    2/3 Revenue generated from incremental sales through
+                    optimized assortment strategies driven by data insights.
+                  </p>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-        <div class="work-section">
-            <div class="mds-container">
-                <div class="mds-m-bottom-5xl mds-font-size-3xl">
-                    <h2>How VusionLive works in-store</h2>
-                </div>
-                <div class="mds-grid mds-grid-size-3 mds-gap-around-3xl">
-                    <div class="work-card mds-grid">
-                        <div class="work-card-image">
-                            <img src="../../../assets/media/products/vusion-live-store.png" alt="" />
-                        </div>
-                        <div class="work-card-content">
-                            <h3 class="mds-font-size-lg mds-m-bottom-xl">Unified Store Management</h3>
-                            <p class="mds-line-height-xl">Consolidates store activities into a single platform, enabling
-                                associates to efficientlty prioritize tasks, manage ESLs, and share data in real time.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="work-card mds-grid">
-                        <div class="work-card-image">
-                            <img src="../../../assets/media/products/vusion-live-campaign.png" alt="" />
-                        </div>
-                        <div class="work-card-content">
-                            <h3 class="mds-font-size-lg mds-m-bottom-xl">Centralized Campaign Execution</h3>
-                            <p class="mds-line-height-xl">Simplifies in-store campaigns, enabling store teams to execute
-                                centrally planned promotions while driving shopper engagement.</p>
-                        </div>
-                    </div>
-                    <div class="work-card mds-grid">
-                        <div class="work-card-image">
-                            <img src="../../../assets/media/products/vusion-live-pricing.png" alt="" />
-                        </div>
-                        <div class="work-card-content">
-                            <h3 class="mds-font-size-lg mds-m-bottom-xl">Data-Driven Pricing Intelligence</h3>
-                            <p class="mds-line-height-xl">Ensures stores are price-compliant and competitive through
-                                actionable insights from real-time data and IoT feedback.</p>
-                        </div>
-                    </div>
-                    <div class="work-card mds-grid">
-                        <div class="work-card-image">
-                            <img src="../../../assets/media/products/vusion-live-inventory.png" alt="" />
-                        </div>
-                        <div class="work-card-content">
-                            <h3 class="mds-font-size-lg mds-m-bottom-xl">Connected Inventory Management</h3>
-                            <p class="mds-line-height-xl">Leverages real-time data to inform and prioritize inventory
-                                decisions for stores, minimizing stockouts.</p>
-                        </div>
-                    </div>
-                    <div class="work-card mds-grid">
-                        <div class="work-card-image">
-                            <img src="../../../assets/media/products/vusion-live-offering.png" alt="" />
-                        </div>
-                        <div class="work-card-content">
-                            <h3 class="mds-font-size-lg mds-m-bottom-xl">Optimized Offering Insights</h3>
-                            <p class="mds-line-height-xl">Uncovers offering gaps and provides local stores with tailored
-                                recommendations to boost sales performance.</p>
-                        </div>
-                    </div>
-                    <div class="work-card mds-grid">
-                        <div class="work-card-image">
-                            <img src="../../../assets/media/products/vusion-live-compliant.png" alt="" />
-                        </div>
-                        <div class="work-card-content">
-                            <h3 class="mds-font-size-lg mds-m-bottom-xl">Compliant Merchandising & Assortment</h3>
-                            <p class="mds-line-height-xl">Enables stores to execute compliant planograms and
-                                assortments, improving visibility and driving in-store sales growth.</p>
-                        </div>
-                    </div>
-                    <div class="work-card mds-grid">
-                        <div class="work-card-image">
-                            <img src="../../../assets/media/products/vusion-live-collaborative.png" alt="" />
-                        </div>
-                        <div class="work-card-content">
-                            <h3 class="mds-font-size-lg mds-m-bottom-xl">Collaborative HQ-Store Workflow</h3>
-                            <p class="mds-line-height-xl">Ensures smooth collaboration between HQ and stores, empowering
-                                store teams to execute on strategies consistently and efficiently.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="why-section work-section">
-            <div class="mds-container">
-                <div class="mds-m-bottom-5xl mds-font-size-3xl">
-                    <h2>Why VusionLive?</h2>
-                </div>
-                <div class="mds-grid mds-grid-size-2 mds-gap-around-5xl">
-                    <div class="mds-flex mds-gap-around-2xl work-card">
-                        <div class="work-card-image">
-                            <img src="../../../assets/media/products/vusion-live-store.png" alt="" />
-                        </div>
-                        <div class="work-card-content">
-                            <h3 class="mds-font-size-lg mds-m-bottom-xl">Optimize Pricing & Promotions</h3>
-                            <p class="mds-line-height-xl">Leverage real-time data from <a
-                                    class="mds-font-weight-600 mds-underline-link" href="#">Electronic Shelf Labels</a>
-                                (ESLs) to monitor competitor pricing, ensure compliance, and react quickly to pricing
-                                shifts with instant alerts, maximizing pricing effectiveness.</p>
-                        </div>
-                    </div>
-                    <div class="mds-flex mds-gap-around-2xl work-card">
-                        <div class="work-card-image">
-                            <img src="../../../assets/media/products/vusion-live-campaign.png" alt="" />
-                        </div>
-                        <div class="work-card-content">
-                            <h3 class="mds-font-size-lg mds-m-bottom-xl">Centralized Campaign Execution</h3>
-                            <p class="mds-line-height-xl">Simplifies in-store campaigns, enabling store teams to execute
-                                centrally planned promotions while driving shopper engagement.</p>
-                        </div>
-                    </div>
-                    <div class="mds-flex mds-gap-around-2xl work-card">
-                        <div class="work-card-image">
-                            <img src="../../../assets/media/products/vusion-live-pricing.png" alt="" />
-                        </div>
-                        <div class="work-card-content">
-                            <h3 class="mds-font-size-lg mds-m-bottom-xl">Data-Driven Pricing Intelligence</h3>
-                            <p class="mds-line-height-xl">Ensures stores are price-compliant and competitive through
-                                actionable insights from real-time data and IoT feedback.</p>
-                        </div>
-                    </div>
-                    <div class="mds-flex mds-gap-around-2xl work-card">
-                        <div class="work-card-image">
-                            <img src="../../../assets/media/products/vusion-live-inventory.png" alt="" />
-                        </div>
-                        <div class="work-card-content">
-                            <h3 class="mds-font-size-lg mds-m-bottom-xl">Connected Inventory Management</h3>
-                            <p class="mds-line-height-xl">Leverages real-time data to inform and prioritize inventory
-                                decisions for stores, minimizing stockouts.</p>
-                        </div>
-                    </div>
-                    <div class="mds-flex mds-gap-around-2xl work-card">
-                        <div class="work-card-image">
-                            <img src="../../../assets/media/products/vusion-live-offering.png" alt="" />
-                        </div>
-                        <div class="work-card-content">
-                            <h3 class="mds-font-size-lg mds-m-bottom-xl">Optimized Offering Insights</h3>
-                            <p class="mds-line-height-xl">Uncovers offering gaps and provides local stores with tailored
-                                recommendations to boost sales performance.</p>
-                        </div>
-                    </div>
-                    <div class="mds-flex mds-gap-around-2xl work-card">
-                        <div class="work-card-image">
-                            <img src="../../../assets/media/products/vusion-live-compliant.png" alt="" />
-                        </div>
-                        <div class="work-card-content">
-                            <h3 class="mds-font-size-lg mds-m-bottom-xl">Compliant Merchandising & Assortment</h3>
-                            <p class="mds-line-height-xl">Enables stores to execute compliant planograms and
-                                assortments, improving visibility and driving in-store sales growth.</p>
-                        </div>
-                    </div>
-                    <div class="mds-flex mds-gap-around-2xl work-card ">
-                        <div class="work-card-image">
-                            <img src="../../../assets/media/products/vusion-live-collaborative.png" alt="" />
-                        </div>
-                        <div class="work-card-content">
-                            <h3 class="mds-font-size-lg mds-m-bottom-xl">Collaborative HQ-Store Workflow</h3>
-                            <p class="mds-line-height-xl">Ensures smooth collaboration between HQ and stores, empowering
-                                store teams to execute on strategies consistently and efficiently.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="mds-section">
-            <div class="mds-container">
-                <div class="mds-flex mds-flex-column">
-                    <div class="mds-grid mds-grid-size-2 mds-gap-around-7xl mds-m-vertical-7xl">
-                        <div class="mds-section-image mds-self-center"><img class="mds-border-radius-base"
-                                src="https://www.vusion.com/wp-content/uploads/2024/12/031A9649-scaled-1-1540x1020-c-default.jpg"
-                                alt=""></div>
-                        <div class="mds-section-content mds-p-around-none">
-                            <h2 class="mds-m-bottom-lg mds-font-size-5xl"> Results </h2>
-                            <div
-                                class="mds-flex mds-flex-column mds-m-top-5xl mds-gap-around-xl mds-p-bottom-2xl mds-height-px-200">
-                                <div class="mds-flex mds-items-center mds-gap-around-xl">
-                                    <i class="pi pi-check mds-font-size-xl"></i>
-                                    <p class="mds-font-weight-600 mds-font-size-lg">
-                                        +12% Volume Growth compared to the same period last year.
-                                    </p>
-                                </div>
-                                <div class="mds-flex mds-items-center mds-gap-around-xl"><i
-                                        class="pi pi-check mds-font-size-xl"></i>
-                                    <p class="mds-font-weight-600 mds-font-size-lg">
-                                        Sales outperformed similar benchmarks by a factor of x7.
-                                    </p>
-                                </div>
-                                <div class="mds-flex mds-items-center mds-gap-around-xl"><i
-                                        class="pi pi-check mds-font-size-xl"></i>
-                                    <p class="mds-font-weight-600 mds-font-size-lg">
-                                        2/3 Revenue generated from incremental sales through optimized assortment
-                                        strategies
-                                        driven by data insights.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
+  </div>
 </template>
